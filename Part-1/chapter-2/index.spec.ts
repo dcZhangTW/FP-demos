2645e3c50ae8901690a7fc3ec236837eac3d46bf
dcZhangTW <dczhang@thoughtworks.com>
2019-08-21 21:18:45 +0800

    Part One: polymophic HOFs

--- Part-1/chapter-2/index.spec.ts
+++ Part-1/chapter-2/index.spec.ts
@@ -1,4 +1,4 @@
-import { factorial, fib } from './index';
+import { factorial, fib, findFirst, findFirstPF, isSorted, curry, uncurry, compose } from './index';
 
 describe('All tests for chapter 2', () => {
   it('should return factorial for input number n', () => {
@@ -10,5 +10,38 @@ describe('All tests for chapter 2', () => {
     expect(fib(1)).toBe(0);
     expect(fib(2)).toBe(1);
     expect(fib(6)).toBe(5);
-  })
+  });
+
+  it('should find first string in string array', () => {
+    const strArr = ['one', 'two', 'three', 'two'];
+    expect(findFirst(strArr, 'one')).toBe(0);
+    expect(findFirst(strArr, 'two')).toBe(1);
+  });
+
+  it('should find first item for any array', () => {
+    const strArr = ['one', 'two', 'three', 'two'];
+    const numArr = [1, 2, 3, 2];
+    expect(findFirstPF(strArr, a => a === 'two')).toBe(1);
+    expect(findFirstPF(numArr, a => a === 2)).toBe(1);
+  });
+
+  it('should check is sorted by sortFun for any array', () => {
+    const sortNum = [1, 2, 3, 4, 5];
+    const nonSortNum = [1, 3, 4, 2, 5];
+    const sortFun = (a1: number, a2: number) => a1 < a2;
+    expect(isSorted(sortNum, sortFun)).toBeTruthy();
+    expect(isSorted(nonSortNum, sortFun)).toBeFalsy();
+  });
+
+  it('should curry & uncurry success', () => {
+    const add = (a: number, b: number) => a + b;
+    const addPlus = (a: number) => (b: number) => a + b;
+    expect(curry(add)(1)(2)).toEqual(add(1, 2));
+    expect(uncurry(addPlus)(1, 2)).toEqual(addPlus(1)(2));
+  });
+
+  it('should compose two function', () => {
+    const toNumber = (a: string) => +a;
+    expect(compose(toNumber, factorial)('5')).toEqual(120);
+  });
 });
--- Part-1/chapter-2/index.ts
+++ Part-1/chapter-2/index.ts
@@ -61,6 +61,45 @@ export function findFirst(ss: string[], key: string): number {
 }
 
 // 对类型进行抽象，不仅仅局限于寻在 string 列表中的某个值如下伪代码
-// export function findFirst<A>(as: A[], p: A => boolean): number {
+export function findFirstPF<A>(as: A[], p: (a: A) => boolean): number {
 
-// }
+  function loop(x: number): number {
+    if (x >= as.length) return -1;
+    else if (p(as[x])) return x;
+    else return loop(x + 1);
+  }
+
+  return loop(0);
+}
+// 通过添加类型变量 A 对类型进行抽象
+
+// 练习：实现方法 isSorted 判断 Array[A] 是否按给定排序方法排序
+export function isSorted<A>(as: A[], p: (a1: A, a2: A) => boolean): boolean {
+
+  function loop(x: number): boolean {
+    if (x === as.length - 1) return true;
+    else {
+      if (p(as[x], as[x + 1])) return loop(x + 1);
+      else return false;
+    }
+  }
+
+  return loop(0);
+}
+
+// 基于行为的进一步抽象，返回另一个函数
+export function partial1<A, B, C>(a: A, f: (a: A, b: B) => C): (b: B) => C {
+  return (b: B) => f(a, b);
+}
+// 练习：curry 方法，将一个接收两个参数的函数，转化为接收一个参数并返回一个接收一个参数的函数的函数
+export function curry<A, B, C>(f: (a: A, b: B) => C): (a: A) => (b: B) => C {
+  return (a: A) => (b: B) => f(a, b);
+}
+// 练习：uncurry
+export function uncurry<A, B, C>(f: (a: A) => (b: B) => C): (a: A, b: B) => C {
+  return (a: A, b: B) => f(a)(b);
+}
+// 练习：compose 方法，将两个方法结合为一个方法
+export function compose<A, B, C>(f1: (a: A) => B, f2: (b: B) => C): (a: A) => C {
+  return (a: A) => f2(f1(a));
+}

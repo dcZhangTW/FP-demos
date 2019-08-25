3a4b8a949a8d6d26507a2ded55c04ede3c107d0f
dcZhangTW <dczhang@thoughtworks.com>
2019-08-25 10:07:20 +0800

    Part One: List functions part one

--- Part-1/chapter-3/index.spec.ts
+++ Part-1/chapter-3/index.spec.ts
@@ -1,9 +1,30 @@
 import { List, asList, equal } from "./index";
-import { tail } from "./utils";
+import { tail, setHead, drop, dropWhile, init } from "./utils";
 
 describe('List data structure', () => {
   it('should remove first item', () => {
     const a: List<number> = asList(1, 2, 3, 4, 5);
     expect(equal(asList(2, 3, 4, 5), tail(a))).toBeTruthy();
+  })
+
+  it('should replace first item', () => {
+    const a: List<number> = asList(2, 3);
+    const b: List<number> = asList(1, 3);
+    expect(equal(b, setHead(a, 1))).toBeTruthy();
+  })
+
+  it('should drop fist n item', () => {
+    const a: List<number> = asList(1, 2, 3, 4);
+    expect(equal(asList(3, 4), drop(a, 2))).toBeTruthy();
+  })
+
+  it('should drop while fit condition', () => {
+    const a: List<number> = asList(1, 2, 3, 4, 5);
+    expect(equal(asList(1, 3, 4, 5), dropWhile(a, (n: number) => n % 2 === 0))).toBeTruthy();
+  })
+
+  it('should remove last item', () => {
+    const a: List<number> = asList(1, 2);
+    expect(equal(asList(1), init(a))).toBeTruthy();
   })
 })
--- Part-1/chapter-3/utils.ts
+++ Part-1/chapter-3/utils.ts
@@ -1,4 +1,4 @@
-import { List, asList, nil } from './index';
+import { List, asList, nil, Cons } from './index';
 // 注意，所有的方法，都必须是纯函数，即不可修改参数数据
 
 // 返回移除第一个元素的List tail
@@ -8,9 +8,43 @@ export function tail<T>(l: List<T>): List<T> {
 }
 
 // 返回替换第一个元素的List setHead
+export function setHead<T>(l: List<T>, head: T): List<T> {
+  if (l.isNil) return nil;
+  else return new Cons(head, l.tail);
+}
 
 // 返回移除前n个元素的List drop，执行效率 O(n)
+export function drop<T>(l: List<T>, n: number): List<T> {
+
+  function go(l: List<T>, x: number): List<T> {
+    if (l.isNil) return nil;
+    else if (x < n) return go(l.tail, x + 1);
+    else return l.tail;
+  }
+
+  return go(l, 1);
+}
 
 // 返回移除符合条件的第一个元素的List dropWhile，执行效率同上
+export function dropWhile<T>(l: List<T>, f: (a: T) => Boolean): List<T> {
+
+  function go(l: List<T>): List<T> {
+    if (l.isNil) return nil;
+    else if (f(l.head)) return l.tail;
+    else return new Cons(l.head, go(l.tail));
+  }
+
+  return go(l);
+}
 
 // 返回移除最后一个元素的List init，这个函数的执行效率如何？
+export function init<T>(l: List<T>): List<T> {
+
+  function go(l: List<T>): List<T> {
+    if (l.isNil) return nil;
+    else if (l.tail.isNil) return nil;
+    else return new Cons(l.head, go(l.tail));
+  }
+
+  return go(l);
+}
